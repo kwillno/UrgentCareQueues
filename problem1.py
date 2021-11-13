feaba0c56532cdc96abf70fa7391477beda8216f
Kristian Willassen <kristian@kwill.no>
2021-11-13 12:36:24 +0100

    Simulation giving expected result.

--- problem1.py
+++ problem1.py
@@ -8,34 +8,57 @@ from scipy import stats as st
 
 def simulateX(lam, mu, t=50):
 
-	timeslices = int(t*24)
+	# Redefining lambda and mu to account for minute timesteps.
+	lam, mu = lam/60, mu/60
+
+	# Setting timesteps to the minutes in each day.
+	timeslices = int(t*24*60)
 	X = np.zeros(timeslices)
 
-	"""
 	# Array for arrivals and departures
 	arrivals = np.random.poisson(lam, (timeslices))
 	departures = np.random.poisson(mu, (timeslices))
 
-	X[0] = 0
+	# Running through all timesteps for simulation
 	for i in range(1,timeslices):
-		X[i] = X[i-1] + arrivals[i] - departures[i]
+		# Setting next X to be previous X + arrivals - departures
+		X[i] = X[i-1] + arrivals[i-1] - departures[i-1]
 		if X[i] < 0:
 			X[i] = 0 
+	
+	
 	"""
-
+	# Attempt 2, not correct either, here takes multiple patients at a time
 	# Array for arrivals	
 	arrivals = np.random.poisson(lam, (timeslices))
 
-	Y = np.zeros(len(X))
+	Y = np.zeros(timeslices)
 	for i in range(1, timeslices):
 		X[i] = X[i-1] + arrivals[i]
-		for j in range(int(X[i])):
+		counter = 0
+		for j in range(int(arrivals[i])):
+			counter += 1
 			try:
-				expTime = np.round(np.random.exponential(mu))
-				X[int(i+expTime)] = X[int(i+expTime)] - 1 
+				expTime = int(np.round(np.random.exponential(mu)))
+				i = i+expTime
+				X[i] = X[i] - 1 
 			except IndexError as e:
 				# print(e)
-				pass
+				break
+	
+	"""
+	"""
+	# Attempt 3
+	# Array for arrivals and departures
+	arrivals = np.random.poisson(lam, (timeslices))
+	departures = np.random.poisson(mu, (timeslices))
+
+	for i in range(1,timeslices):
+		X[i] = X[i-1] + arrivals[i-1] - departures[i-1]
+		if X[i] < 0:
+			print(X[i])
+			X[i] = 0
+	"""
 
 	return X
 
@@ -47,7 +70,7 @@ def plotRealizations(n, lam, mu, t=50):
 		plt.plot(X)
 	plt.show()
 
-# plotRealizations(1, 5, 6, t = 5)
+# plotRealizations(2, 5, 6, t = 10)
 
 # Problem b, 2
 # Finding expected time a patient spends in the UCC
@@ -84,13 +107,15 @@ def getCI(lam, mu, percentage=0.95):
 	CI = st.norm.interval(0.95, loc=np.mean(waitTimes), scale=st.sem(waitTimes))
 	print(f"Wait times 95% CI: {CI[0]*60:.2f} - {CI[1]*60:.2f} mins, {CI} hours")
 
-exactWaitTime = lambda lam, mu : 1/(mu-lam)
-
 getCI(5,6)
 
-waitHour = exactWaitTime(5,6)
-waitMin = waitHour*60
-print(f"Exact wait time: {waitHour:.4f} hours, {waitMin:.2f}")
+exactWaitTime = lambda lam, mu : 1/(mu-lam)
+def getExactWaitTime(lam, mu):
+	waitHour = exactWaitTime(lam,mu)
+	waitMin = waitHour*60
+	print(f"Exact wait time: {waitHour:.4f} hours, {waitMin:.2f}")
+
+# getExactWaitTime(5,6)
 
 # Problem b, 4
 # Plot one realization of {X(t) : t ≥ 0} for the time 0–12 hours.
@@ -108,8 +133,8 @@ def plotRealizationX(lam=5, mu=6):
 	plt.title("Realization of $X(t)$")
 	for x,y in curves:
 		plt.plot(x,y,"b")
-	plt.xlabel("Hours")
-	plt.xlim([-0.4,12.4])
+	# plt.xlabel("Hours")
+	# plt.xlim([-0.4,12.4])
 	plt.ylabel("Patients")
 	plt.show()
 
