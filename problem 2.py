451ba2e4ea062dee18b5b24bc98014cb90658d42
Kristian Willassen <kristian@kwill.no>
2021-11-17 01:20:43 +0100

    Final commit. Black formatted.

--- problem 2.py
+++ problem 2.py
@@ -1,7 +1,7 @@
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 from numpy import lib, nan
-from scipy.stats import norm 
+from scipy.stats import norm
 from scipy.linalg import inv
 
 
@@ -11,11 +11,16 @@ PROBLEM 2a
 
 # ARRAYS AND VARIABLES
 
-grid = np.arange(0.25, 0.505, 0.005) # an array with 51 elements from 0.25 to 0.50
-xB = np.array([0.30, 0.35, 0.39, 0.41, 0.45]) # measured values of theta given in the problem sheet
-yB = np.array([0.5, 0.32, 0.40, 0.35, 0.60]) # measured values of y(theta) given in the problem sheet
+grid = np.arange(0.25, 0.505, 0.005)  # an array with 51 elements from 0.25 to 0.50
+xB = np.array(
+    [0.30, 0.35, 0.39, 0.41, 0.45]
+)  # measured values of theta given in the problem sheet
+yB = np.array(
+    [0.5, 0.32, 0.40, 0.35, 0.60]
+)  # measured values of y(theta) given in the problem sheet
 
 # FUNCTIONS
+
 
 def make_mu(t, m=0.5):
     """
@@ -28,32 +33,33 @@ def make_mu(t, m=0.5):
     return mu
 
 
-
 def r(t1, t2):
     """
     This is the corrolation-function implementes as given in the problem-sheet.
     """
-    return (1+15*abs(t1-t2))*np.exp(-15*abs(t1-t2))
+    return (1 + 15 * abs(t1 - t2)) * np.exp(-15 * abs(t1 - t2))
 
 
-
-def C(t1, t2, s=0.5**2):
+def C(t1, t2, s=0.5 ** 2):
     """
     This is the covariance-function impemented as given in the problem-sheet
     """
-    return s*r(t1, t2)
-
+    return s * r(t1, t2)
 
 
 def make_standard_sigma(xB):
     """
     Uses the covariance-function to initialize aech component of the sigma-matrix
     """
-    sigma = np.zeros((len(xB), len(xB))) # a matrix where we are going to place the values of sigma
+    sigma = np.zeros(
+        (len(xB), len(xB))
+    )  # a matrix where we are going to place the values of sigma
     n = len(sigma)
     for i in range(n):
         for j in range(n):
-            sigma[i,j] = C(xB[i], xB[j]) # each element of sigma are to be implemented with covariance-function
+            sigma[i, j] = C(
+                xB[i], xB[j]
+            )  # each element of sigma are to be implemented with covariance-function
     return sigma
 
 
@@ -61,23 +67,29 @@ def index(tB, t):
     """
     returns the boolean-indicies of where the theta-values from tB are in t
     """
-    new_tB = np.zeros(len(t), dtype=bool) # new array to hold boolean values
+    new_tB = np.zeros(len(t), dtype=bool)  # new array to hold boolean values
     for i in range(len(t)):
-        new_tB[i] = any(abs(tB-t[i])<1e-10) # uses any() to check if the i_th element of t is in tB
+        new_tB[i] = any(
+            abs(tB - t[i]) < 1e-10
+        )  # uses any() to check if the i_th element of t is in tB
     return new_tB
 
 
 def make_mu_sigma_C():
     """
-    This function constructs all the relevant submatricies of sigma, so we can use them 
-    to construct the conditional mu and sigma matricies. 
+    This function constructs all the relevant submatricies of sigma, so we can use them
+    to construct the conditional mu and sigma matricies.
     """
-    inds = index(xB, grid) # inds is a boolean array where every element of grid that is in xB is True
-    inds_Complement = np.invert(inds) # inverts inds so Truth becomes False and vice verca
+    inds = index(
+        xB, grid
+    )  # inds is a boolean array where every element of grid that is in xB is True
+    inds_Complement = np.invert(
+        inds
+    )  # inverts inds so Truth becomes False and vice verca
 
-    #construction of the original muA, muB and sigma
+    # construction of the original muA, muB and sigma
     muA = make_mu(grid)
-    muB = make_mu(xB) 
+    muB = make_mu(xB)
     sigma = make_standard_sigma(grid)
 
     # Construction of the components of mu_C and sigma_C
@@ -88,12 +100,15 @@ def make_mu_sigma_C():
     sigma_AA = sigma[np.ix_(inds_Complement, inds_Complement)]
     sigma_BA = sigma[np.ix_(inds, inds_Complement)]
 
-
     # the making of mu_C and sigma_C
-    #mu_C:
-    mu_C = mu_A + sigma_AB @ np.linalg.solve(sigma_BB, (yB - muB)) # we use the formula for conditional mu 
-    #sigma_C
-    a = np.matmul(inv(sigma_BB), sigma_BA) # in these lines we du much the same as above, but in more steps
+    # mu_C:
+    mu_C = mu_A + sigma_AB @ np.linalg.solve(
+        sigma_BB, (yB - muB)
+    )  # we use the formula for conditional mu
+    # sigma_C
+    a = np.matmul(
+        inv(sigma_BB), sigma_BA
+    )  # in these lines we du much the same as above, but in more steps
     b = np.matmul(sigma_AB, a)
     sigma_C = sigma_AA - b
 
@@ -106,7 +121,7 @@ def predInt(i, var_C):
     """
     Returns 90% prediciton interval for theta_i
     """
-    return 1.645*np.sqrt(var_C[i])
+    return 1.645 * np.sqrt(var_C[i])
 
 
 def make_conf_intervalls(mu, var):
@@ -118,11 +133,11 @@ def make_conf_intervalls(mu, var):
     for i, mu_i in np.ndenumerate(mu):
         upper[i], lower[i] = mu_i + predInt(i, var), mu_i - predInt(i, var)
     return upper, lower
- 
+
 
 def plot_mu_C():
     """
-    In this function we use the functions above to initialize conditional mu and conditional var, and use these 
+    In this function we use the functions above to initialize conditional mu and conditional var, and use these
     arrays toughether with grid (that holds the theta-values) to plot conditional theta and a 90% prediction
     interval.
     """
@@ -132,14 +147,14 @@ def plot_mu_C():
     mu_C_new = np.insert(mu_C, ind_values, yB)
     var_C = np.insert(var_C, ind_values, np.zeros(len(ind_values)))
 
-    # here we construct the upper and lower limits of the 90% confidence interval 
+    # here we construct the upper and lower limits of the 90% confidence interval
     upper, lower = make_conf_intervalls(mu_C_new, var_C)
 
     # here we plot the confidence-interval toughether with conditional mu
-    plt.figure(figsize=(10,6))
-    plt.plot(xB, yB,'o')
-    plt.plot(grid, mu_C_new, color='b', label='Conditional mean')
-    plt.fill_between(grid, lower, upper, color='b', alpha=0.1)
+    plt.figure(figsize=(10, 6))
+    plt.plot(xB, yB, "o")
+    plt.plot(grid, mu_C_new, color="b", label="Conditional mean")
+    plt.fill_between(grid, lower, upper, color="b", alpha=0.1)
     plt.title("Prediction, with 90% prediction interval")
     plt.xlabel("$\\theta$")
     plt.ylabel("$Y(\\theta)$")
@@ -147,14 +162,14 @@ def plot_mu_C():
     plt.show()
 
 
-
 """ 
 PROBLEM 2B
 """
 
+
 def problem_2_b():
     """
-    here we use norm.cdf to find the conditional probability that y(theta)<0.30. 
+    here we use norm.cdf to find the conditional probability that y(theta)<0.30.
     We call the array with those values cd_val, and plot them as a function of theta.
     """
 
@@ -163,11 +178,13 @@ def problem_2_b():
     ind_values = np.array([i for i in range(len(inds)) if inds[i]])
     ind_values -= np.arange(len(ind_values))
     mu_C_new = np.insert(mu_C, ind_values, yB)
-    var_C = np.insert(var_C, ind_values, 1e-5*np.ones(len(ind_values)))
+    var_C = np.insert(var_C, ind_values, 1e-5 * np.ones(len(ind_values)))
 
-    cd_vals = norm.cdf(0.3*np.ones_like(mu_C_new), mu_C_new, np.sqrt(var_C)) # use norm.cdf to find cd
+    cd_vals = norm.cdf(
+        0.3 * np.ones_like(mu_C_new), mu_C_new, np.sqrt(var_C)
+    )  # use norm.cdf to find cd
 
-    # down here we plot 
+    # down here we plot
     plt.plot(grid, cd_vals)
     plt.title("Cond. Prob. Y(θ) < 0.30")
     plt.xlabel("$\\theta$")
@@ -184,8 +201,9 @@ plot the same things as in a) and b), but with a point added to grid
 """
 
 # these are the same values as before, but with the point (0.33, 0.40) added
-xB = np.array([0.30, 0.33, 0.35, 0.39, 0.41, 0.45]) 
-yB = np.array([0.5, 0.40, 0.32, 0.40, 0.35, 0.60])  
+xB = np.array([0.30, 0.33, 0.35, 0.39, 0.41, 0.45])
+yB = np.array([0.5, 0.40, 0.32, 0.40, 0.35, 0.60])
+
 
 def problem_2_c1():
 
@@ -200,15 +218,16 @@ def problem_2_c1():
     upper, lower = make_conf_intervalls(mu_C_new, var_C)
 
     # here we plot it all
-    plt.figure(figsize=(10,6))
-    plt.plot(xB, yB,'o')
-    plt.plot(grid, mu_C_new, color='b', label='Conditional mean')
-    plt.fill_between(grid, lower, upper, color='b', alpha=0.1)
+    plt.figure(figsize=(10, 6))
+    plt.plot(xB, yB, "o")
+    plt.plot(grid, mu_C_new, color="b", label="Conditional mean")
+    plt.fill_between(grid, lower, upper, color="b", alpha=0.1)
     plt.grid()
     plt.title("Prediction, with 90% PI, using $6$ points")
     plt.xlabel("$\\theta$")
     plt.ylabel("$Y(\\theta)$")
     plt.show()
+
 
 def problem_2_c2():
     # We construct the mu_C values
@@ -216,17 +235,17 @@ def problem_2_c2():
     ind_values = np.array([i for i in range(len(inds)) if inds[i]])
     ind_values -= np.arange(len(ind_values))
     mu_C_new = np.insert(mu_C, ind_values, yB)
-    var_C = np.insert(var_C, ind_values, 1e-5*np.ones(len(ind_values)))
+    var_C = np.insert(var_C, ind_values, 1e-5 * np.ones(len(ind_values)))
 
-    # here we construct the confidence interval 
+    # here we construct the confidence interval
     upper, lower = make_conf_intervalls(mu_C_new, var_C)
 
-    cd_vals = norm.cdf(0.3*np.ones_like(mu_C_new), mu_C_new, np.sqrt(var_C))
+    cd_vals = norm.cdf(0.3 * np.ones_like(mu_C_new), mu_C_new, np.sqrt(var_C))
 
     maks = np.amax(cd_vals)
     maks_index = np.argmax(cd_vals)
 
-    print(f'max value: {maks:.4f}, index of max value: {grid [maks_index]:.2f}')
+    print(f"max value: {maks:.4f}, index of max value: {grid [maks_index]:.2f}")
 
     plt.plot(grid, cd_vals)
     plt.title("P{Y(θ) < 0.30} with $6$ points")
@@ -234,7 +253,6 @@ def problem_2_c2():
     plt.ylabel("P{Y(θ) < 0.30}")
     plt.grid()
     plt.show()
-
 
 
 def main():
@@ -245,6 +263,6 @@ def main():
 
     problem_2_c1()
     problem_2_c2()
-    
-main() 
 
+
+main()
--- problem1.py
+++ problem1.py
@@ -6,55 +6,57 @@ from scipy import stats as st
 # Problem b, 1
 # Simulate X(t)
 
+
 def simulateX(lam, mu, t=50):
-	"""
-	Function to create a realization of X(t).
-	"""
+    """
+    Function to create a realization of X(t).
+    """
 
-	# Redefining lambda and mu to account for minute timesteps.
-	lam, mu = lam/60, mu/60
+    # Redefining lambda and mu to account for minute timesteps.
+    lam, mu = lam / 60, mu / 60
 
-	# Setting timesteps to the minutes in each day.
-	N = int(t*24*60)
-	X = np.zeros(N)
-	t = np.zeros(N)
+    # Setting timesteps to the minutes in each day.
+    N = int(t * 24 * 60)
+    X = np.zeros(N)
+    t = np.zeros(N)
 
-	# Running through all timesteps for simulation
-	for i in range(1,N):
+    # Running through all timesteps for simulation
+    for i in range(1, N):
 
-		# Getting exponentially distributed sojourn time
-		# and blocking action until it ends.
-		S = int(np.random.exponential(lam + mu))
-		block = t[i-1] + S - 1
+        # Getting exponentially distributed sojourn time
+        # and blocking action until it ends.
+        S = int(np.random.exponential(lam + mu))
+        block = t[i - 1] + S - 1
 
-		# Action when updating state.
-		if t[i] > block:
-			u = np.random.uniform()
-			if u < lam/(lam + mu):
-				X[i] = X[i-1] + 1
-			else:
-				X[i] = X[i-1] - 1
-		else:
-			X[i] = X[i-1]
+        # Action when updating state.
+        if t[i] > block:
+            u = np.random.uniform()
+            if u < lam / (lam + mu):
+                X[i] = X[i - 1] + 1
+            else:
+                X[i] = X[i - 1] - 1
+        else:
+            X[i] = X[i - 1]
 
-		# Making sure state is nonnegative
-		if X[i] < 0:
-			X[i] = 0
+        # Making sure state is nonnegative
+        if X[i] < 0:
+            X[i] = 0
 
-	return X
+    return X
 
 
 def plotRealizations(n, lam, mu, t=50):
-	"""
-	Testing function.
+    """
+    Testing function.
 
-	Plots multiple(n) realizations of X(t)
-	"""
-	plt.figure("Test")
-	for i in range(n):
-		X = simulateX(lam, mu, t)
-		plt.plot(X)
-	plt.show()
+    Plots multiple(n) realizations of X(t)
+    """
+    plt.figure("Test")
+    for i in range(n):
+        X = simulateX(lam, mu, t)
+        plt.plot(X)
+    plt.show()
+
 
 # Problem b, 2
 # Finding expected time a patient spends in the UCC
@@ -66,231 +68,250 @@ expectedTime = lambda avgCustomers, arrivalRate: avgCustomers / arrivalRate
 # Problem b, 3
 # Finding 95% CI
 
+
 def getCI(lam, mu, percentage=0.95):
-	"""
-	Function for finding confidence interval of wait time using
-	a realization of X(t).
-	"""
-	waitTimes = np.zeros(30)
-	for i in range(len(waitTimes)):
-		X = simulateX(lam, mu, t = 50)
-		waitTimes[i] = expectedTime(np.average(X),lam)
+    """
+    Function for finding confidence interval of wait time using
+    a realization of X(t).
+    """
+    waitTimes = np.zeros(30)
+    for i in range(len(waitTimes)):
+        X = simulateX(lam, mu, t=50)
+        waitTimes[i] = expectedTime(np.average(X), lam)
 
-	print("Normal distribution")
-	CI = st.norm.interval(percentage, loc=np.mean(waitTimes), scale=st.sem(waitTimes))
-	print(f"Wait times {percentage*10:.0f}% CI: {CI[0]*60:.2f} - {CI[1]*60:.2f} mins, {CI[0]:.4f} - {CI[1]:.4f} hours")
+    print("Normal distribution")
+    CI = st.norm.interval(percentage, loc=np.mean(waitTimes), scale=st.sem(waitTimes))
+    print(
+        f"Wait times {percentage*10:.0f}% CI: {CI[0]*60:.2f} - {CI[1]*60:.2f} mins, {CI[0]:.4f} - {CI[1]:.4f} hours"
+    )
 
 
-exactWaitTime = lambda lam, mu : 1/(mu-lam)
+exactWaitTime = lambda lam, mu: 1 / (mu - lam)
+
+
 def getExactWaitTime(lam, mu):
-	"""
-	Gets the exact wait time using the analytical solution.
-	"""
-	waitHour = exactWaitTime(lam,mu)
-	waitMin = waitHour*60
-	print(f"Exact wait time: {waitHour:.4f} hours, {waitMin:.2f}")
+    """
+    Gets the exact wait time using the analytical solution.
+    """
+    waitHour = exactWaitTime(lam, mu)
+    waitMin = waitHour * 60
+    print(f"Exact wait time: {waitHour:.4f} hours, {waitMin:.2f}")
+
 
 # Problem b, 4
 # Plot one realization of {X(t) : t ≥ 0} for the time 0–12 hours.
 
 
 def plotRealizationX(lam=5, mu=6):
-	"""
-	Plots one realization of X(t) as a discrete-plot where
-	the functions are not continous. 
-	"""
-	X = simulateX(lam, mu, t = 1/2)
-	curves = []
-	i = 1
-	while i < len(X):
-		curves.append(([i-1,i], [X[i-1], X[i-1]]))
-		i = i+1 
+    """
+    Plots one realization of X(t) as a discrete-plot where
+    the functions are not continous.
+    """
+    X = simulateX(lam, mu, t=1 / 2)
+    curves = []
+    i = 1
+    while i < len(X):
+        curves.append(([i - 1, i], [X[i - 1], X[i - 1]]))
+        i = i + 1
 
-	plt.figure("Try")
-	plt.title("Realization of $X(t)$")
-	for x,y in curves:
-		x,y = np.array(x), np.array(y)
-		plt.plot(x/60,y,"b")
-	plt.xlabel("Hours")
-	plt.xlim([-0.4,12.4])
-	plt.ylabel("Patients")
-	plt.show()
+    plt.figure("Try")
+    plt.title("Realization of $X(t)$")
+    for x, y in curves:
+        x, y = np.array(x), np.array(y)
+        plt.plot(x / 60, y, "b")
+    plt.xlabel("Hours")
+    plt.xlim([-0.4, 12.4])
+    plt.ylabel("Patients")
+    plt.show()
 
 
 # ---------------------------------
 # Problem 1f
 
-W_U = lambda p, lam, mu : 1/(mu-p*lam) 
-W_N = lambda p, lam, mu : mu/((mu - lam)*(mu - p*lam))
+W_U = lambda p, lam, mu: 1 / (mu - p * lam)
+W_N = lambda p, lam, mu: mu / ((mu - lam) * (mu - p * lam))
+
 
 def plotWaits(lam, mu):
-	"""
-	Plots the analytical functions W_U(p) and W_N(p) 
-	with given lambda and mu.
-	"""
-	p = np.linspace(0,1)
+    """
+    Plots the analytical functions W_U(p) and W_N(p)
+    with given lambda and mu.
+    """
+    p = np.linspace(0, 1)
 
-	W_u = W_U(p, lam, mu)
-	W_n = W_N(p, lam, mu)
+    W_u = W_U(p, lam, mu)
+    W_n = W_N(p, lam, mu)
 
-	plt.figure("waitTimes")
-	plt.title("Expected time in UCC")
-	plt.plot(p,W_u,label="$W_U$ - Urgent patients")
-	plt.plot(p,W_n,label="$W_N$ - Normal patients")
-	plt.xlabel("Probability that patient is urgent")
-	plt.ylabel("Hours")
-	plt.legend()
-	plt.show()
+    plt.figure("waitTimes")
+    plt.title("Expected time in UCC")
+    plt.plot(p, W_u, label="$W_U$ - Urgent patients")
+    plt.plot(p, W_n, label="$W_N$ - Normal patients")
+    plt.xlabel("Probability that patient is urgent")
+    plt.ylabel("Hours")
+    plt.legend()
+    plt.show()
+
 
 def getWaitTimeExtremes(lam, mu):
-	print(f"Expected wait time for a normal patient is {W_N(0,lam,mu)}, p~0. ")
-	print(f"Expected wait time for a normal patient is {W_N(1,lam,mu)}, p~1. ")
-
+    print(f"Expected wait time for a normal patient is {W_N(0,lam,mu)}, p~0. ")
+    print(f"Expected wait time for a normal patient is {W_N(1,lam,mu)}, p~1. ")
 
 
 # ---------------------------
 # problem 1g
 
+
 def muFnc(u, n, mu):
-	"""
-	Helperfunction for simulating realizations of U(t) and N(t).
-	"""
-	if u+n == 0:
-		return 0
-	return mu
+    """
+    Helperfunction for simulating realizations of U(t) and N(t).
+    """
+    if u + n == 0:
+        return 0
+    return mu
 
 
 def simulateUN(p, lam, mu, t=50):
-	"""
-	Function to create a realization of U(t) and N(t).
-	"""
+    """
+    Function to create a realization of U(t) and N(t).
+    """
 
-	# Redefining lambda and mu to account for minute timesteps.
-	lam, mu = lam/60, mu/60
-	
-	# Setting timesteps to the minutes in each day.
-	# Creating needed arrays and fill with zeros.
-	timeslices = int(t*24*60)
-	U = np.zeros(timeslices)
-	N = np.zeros(timeslices)
-	block = 0
+    # Redefining lambda and mu to account for minute timesteps.
+    lam, mu = lam / 60, mu / 60
 
-	# Running through all timesteps for simulation
-	for i in range(1, timeslices):
-		U[i] = U[i-1]
-		N[i] = N[i-1]
+    # Setting timesteps to the minutes in each day.
+    # Creating needed arrays and fill with zeros.
+    timeslices = int(t * 24 * 60)
+    U = np.zeros(timeslices)
+    N = np.zeros(timeslices)
+    block = 0
 
-		# Blocking action until iteration given by block.
-		if not (i < block):
+    # Running through all timesteps for simulation
+    for i in range(1, timeslices):
+        U[i] = U[i - 1]
+        N[i] = N[i - 1]
 
-			# Getting a valid mu for i'th iteration
-			mu_i = muFnc(U[i-1], N[i-1], mu)
+        # Blocking action until iteration given by block.
+        if not (i < block):
 
-			# Getting exponentially distributed sojourn time
-			S = (np.random.exponential((mu_i + lam)**(-1)))
-			block = i + S
+            # Getting a valid mu for i'th iteration
+            mu_i = muFnc(U[i - 1], N[i - 1], mu)
 
-			# Finding increment/decrement irrespective of U/N
-			change = np.random.choice([-1,1], p=( mu_i/(mu_i + lam) , lam/(mu_i + lam) ))
+            # Getting exponentially distributed sojourn time
+            S = np.random.exponential((mu_i + lam) ** (-1))
+            block = i + S
 
-			if ( # All the possible situations for change in N(t)
-				(U[i-1] == 0 and change == -1) or 
-				(np.random.uniform() > p and change == 1)
-			):
-				N[i] += change
-			else:
-				U[i] += change
+            # Finding increment/decrement irrespective of U/N
+            change = np.random.choice(
+                [-1, 1], p=(mu_i / (mu_i + lam), lam / (mu_i + lam))
+            )
 
-	return U,N
+            if (  # All the possible situations for change in N(t)
+                U[i - 1] == 0 and change == -1
+            ) or (np.random.uniform() > p and change == 1):
+                N[i] += change
+            else:
+                U[i] += change
+
+    return U, N
+
 
 def plotRealizationsUN(n, p, lam, mu, t=50):
-	"""
-	Testing function.
+    """
+    Testing function.
 
-	Plots multiple(n) realizations of U(t) and N(t)
-	"""
-	plt.figure("Test")
-	for i in range(n):
-		U, N = simulateUN(p, lam, mu, t)
-		plt.plot(U, "b", label="Urgent")
-		plt.plot(N, "r", label="Normal")
-	plt.grid()
-	plt.legend()
-	plt.show()
+    Plots multiple(n) realizations of U(t) and N(t)
+    """
+    plt.figure("Test")
+    for i in range(n):
+        U, N = simulateUN(p, lam, mu, t)
+        plt.plot(U, "b", label="Urgent")
+        plt.plot(N, "r", label="Normal")
+    plt.grid()
+    plt.legend()
+    plt.show()
 
 
 def plotRealizationUN(p=0.8, lam=5, mu=6, t=1):
-	"""
-	Plots one realization of X(t) as a discrete-plot where
-	the functions U(t) and N(t)are not continous. 
-	"""
-	# Completing simulation
-	U, N = simulateUN(p, lam, mu, t)
+    """
+    Plots one realization of X(t) as a discrete-plot where
+    the functions U(t) and N(t)are not continous.
+    """
+    # Completing simulation
+    U, N = simulateUN(p, lam, mu, t)
 
-	# Transform to discrete functions
-	curvesU = []
-	curvesN = []
-	i = 1
-	while i < len(U):
-		curvesU.append(([i-1,i], [U[i-1], U[i-1]]))
-		curvesN.append(([i-1,i], [N[i-1], N[i-1]]))
-		i = i+1 
+    # Transform to discrete functions
+    curvesU = []
+    curvesN = []
+    i = 1
+    while i < len(U):
+        curvesU.append(([i - 1, i], [U[i - 1], U[i - 1]]))
+        curvesN.append(([i - 1, i], [N[i - 1], N[i - 1]]))
+        i = i + 1
 
-	# Big scary plotting mess
-	plt.figure("Try")
-	plt.title("Realization of $U(t)$ and $N(t)$")
+    # Big scary plotting mess
+    plt.figure("Try")
+    plt.title("Realization of $U(t)$ and $N(t)$")
 
-	# Plots fot U(t)
-	plt.plot(0,0,"b",label="$U(t)$")
-	for x,y in curvesU:
-		x,y = np.array(x), np.array(y)
-		plt.plot(x/60,y,"b")
+    # Plots fot U(t)
+    plt.plot(0, 0, "b", label="$U(t)$")
+    for x, y in curvesU:
+        x, y = np.array(x), np.array(y)
+        plt.plot(x / 60, y, "b")
 
-	# Plots fot N(t)
-	plt.plot(0,0,"r",label="$N(t)$")
-	for x,y in curvesN:
-		x,y = np.array(x), np.array(y)
-		plt.plot(x/60,y,"r")
+    # Plots fot N(t)
+    plt.plot(0, 0, "r", label="$N(t)$")
+    for x, y in curvesN:
+        x, y = np.array(x), np.array(y)
+        plt.plot(x / 60, y, "r")
 
-	plt.xlabel("Hours")
-	plt.ylabel("Patients")
-	plt.legend()
-	plt.show()
+    plt.xlabel("Hours")
+    plt.ylabel("Patients")
+    plt.legend()
+    plt.show()
 
 
 def getCI_UN(p, lam, mu, percentage=0.95):
-	"""
-	Function for finding confidence interval of wait time using
-	a realization of U(t) and N(t).
-	Gives separate Confidence intervals for U(t) and N(t).
-	"""
-	waitTimesU = np.zeros(30)
-	waitTimesN = np.zeros(30)
+    """
+    Function for finding confidence interval of wait time using
+    a realization of U(t) and N(t).
+    Gives separate Confidence intervals for U(t) and N(t).
+    """
+    waitTimesU = np.zeros(30)
+    waitTimesN = np.zeros(30)
 
-	for i in range(len(waitTimesU)):
-		U, N = simulateUN(p, lam, mu, t = 50)
-		waitTimesU[i] = expectedTime(np.average(U),p*lam)
-		waitTimesN[i] = expectedTime(np.average(N),(1-p)*lam)
+    for i in range(len(waitTimesU)):
+        U, N = simulateUN(p, lam, mu, t=50)
+        waitTimesU[i] = expectedTime(np.average(U), p * lam)
+        waitTimesN[i] = expectedTime(np.average(N), (1 - p) * lam)
 
-	print("Normal distribution")
-	CI_U = st.norm.interval(percentage, loc=np.mean(waitTimesU), scale=st.sem(waitTimesU))
-	print(f"Wait times {percentage*10:.0f}% CI Urgent: {CI_U[0]*60:.2f} - {CI_U[1]*60:.2f} mins, {CI_U[0]:.4f} - {CI_U[1]:.4f} hours")
+    print("Normal distribution")
+    CI_U = st.norm.interval(
+        percentage, loc=np.mean(waitTimesU), scale=st.sem(waitTimesU)
+    )
+    print(
+        f"Wait times {percentage*10:.0f}% CI Urgent: {CI_U[0]*60:.2f} - {CI_U[1]*60:.2f} mins, {CI_U[0]:.4f} - {CI_U[1]:.4f} hours"
+    )
 
-	CI_N = st.norm.interval(percentage, loc=np.mean(waitTimesN), scale=st.sem(waitTimesN))
-	print(f"Wait times {percentage*10:.0f}% CI Normal: {CI_N[0]*60:.2f} - {CI_N[1]*60:.2f} mins, {CI_N[0]:.4f} - {CI_N[1]:.4f} hours")
+    CI_N = st.norm.interval(
+        percentage, loc=np.mean(waitTimesN), scale=st.sem(waitTimesN)
+    )
+    print(
+        f"Wait times {percentage*10:.0f}% CI Normal: {CI_N[0]*60:.2f} - {CI_N[1]*60:.2f} mins, {CI_N[0]:.4f} - {CI_N[1]:.4f} hours"
+    )
 
 
 def getExact_UN(p=0.8, lam=5, mu=6):
-	"""
-	Gets the exact wait time using the analytical solution.
-	These are here given by W_U and W_N functions defined above.
-	"""
-	Wu = W_U(p, lam, mu)
-	Wn = W_N(p, lam, mu)
+    """
+    Gets the exact wait time using the analytical solution.
+    These are here given by W_U and W_N functions defined above.
+    """
+    Wu = W_U(p, lam, mu)
+    Wn = W_N(p, lam, mu)
 
-	WuMin, WnMin = Wu*60, Wn*60
-	print(f"Exact wait time: {WuMin:.2f} minutes, {Wu:.4f} hours")
-	print(f"Exact wait time: {WnMin:.2f} minutes, {Wn:.4f} hours")
+    WuMin, WnMin = Wu * 60, Wn * 60
+    print(f"Exact wait time: {WuMin:.2f} minutes, {Wu:.4f} hours")
+    print(f"Exact wait time: {WnMin:.2f} minutes, {Wn:.4f} hours")
+
 
 # Main section
 # --------------------------------------------------
@@ -300,18 +321,18 @@ lam, mu = 5, 6
 
 plotRealizations(2, lam, mu)
 
-getCI(lam,mu)
+getCI(lam, mu)
 
-getExactWaitTime(lam,mu)
+getExactWaitTime(lam, mu)
 
 plotRealizationX(lam, mu)
 
-plotWaits(lam,mu)
+plotWaits(lam, mu)
 getWaitTimeExtremes(lam, mu)
 
-plotRealizationsUN(1, 0.8, lam, mu, t=1/8)
+plotRealizationsUN(1, 0.8, lam, mu, t=1 / 8)
 
-plotRealizationUN(t=1/2)
+plotRealizationUN(t=1 / 2)
 
 getCI_UN(0.8, lam, mu)
 
